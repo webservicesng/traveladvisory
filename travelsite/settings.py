167a6278c3d4bc67e27a8e96825b043127c4a388
fkexo <frank4everreal@gmail.com>
2024-05-11 10:09:01 +0100

    added the .env and .ignore configurationn

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,13 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ffm$c$&r2zv6zg2s$mx*4zw7*pt8ze777jbn)qvwslnkwd(e3'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1', '::1'
+    ]
 
 # Application definition
 
