c5f1d25994c2d325563f435a0c4c569898ebf864
fkexo <frank4everreal@gmail.com>
2024-05-13 23:43:07 +0100

    ssss

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost', '127.0.0.1', '::1', 'https://travelsite-svbl.onrender.com'
