76e3b97aad1be16daf282b51042e349324fe9031
fkexo <frank4everreal@gmail.com>
2024-05-13 22:34:20 +0100

    done with render deployment set up

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.whiteNoiseMidleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,6 +136,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+# added the staticfiles_storage for compresing and catching of our staticfiles
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
