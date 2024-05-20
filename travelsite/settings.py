97244847ae6d5347f3e6112f40f5c9fa6fbcc1d2
fkexo <frank4everreal@gmail.com>
2024-05-20 19:46:35 +0100

    emited stage file

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('DEBUG', 'False') == "False"
+DEBUG = os.environ.get('DEBUG', 'True') == "True"
 print(DEBUG)
 
 ALLOWED_HOSTS = [
