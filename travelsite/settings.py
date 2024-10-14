f04e32d5fec129edfca345f9272d4ba5d1699ecf
fkexo <frank4everreal@gmail.com>
2024-10-14 02:01:17 +0100

    added a new database

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('DEBUG', 'True') == "True"
+DEBUG = os.environ.get('DEBUG', 'False') == "False"
 print(DEBUG)
 
 ALLOWED_HOSTS = [
